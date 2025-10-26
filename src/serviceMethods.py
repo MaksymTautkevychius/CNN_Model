524b0f29bd09ec5d7556f4fd227396ce92d11793
Maksym <maksymtautkevychius@gmail.com>
2025-10-26 17:01:09 +0100

    seerviceMethods renamed serviceMethods

---
+++ src/serviceMethods.py
@@ -0,0 +1,10 @@
+import torch
+def print_train_time(start, end, device=None):
+    total_time = end - start
+    print(f"\nTrain time on {device}: {total_time:.3f} seconds")
+    return total_time
+
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct / len(y_pred)) * 100
+    return acc
